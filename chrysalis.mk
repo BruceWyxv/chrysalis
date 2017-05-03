5f4853b231fbd6ccac7667919bbb870a686632b5
Brycen Wendt <wendbryc@isu.edu>
2017-05-03 13:45:11 -0600

    Created a single-step build process
    The makefile calls the script to copy/update the Serpent files, then builds the library in the proper sequence
    When this is implemented as a module in MOOSE then the global module makefile will need to have a check for SERPENT_HOME in order to trigger the build of this module

--- chrysalis.mk
+++ chrysalis.mk
@@ -1,31 +1,95 @@
 # Build Serpent
 ###############################################################################
 
-# Set up the environment
+# Ensure a sane build environment, error if otherwise
 
-NAME 			:= serpent
-SERPENT_DIR 		:= $(CURDIR)/serpent
-SERPENT_LIB		:= $(SERPENT_DIR)/lib$(NAME)-$(METHOD).la
-SERPENT_SRC 		:= $(sort $(wildcard $(SERPENT_DIR)/*.c))
+define MESSAGE_ERROR
+
+================================================================================
+SERPENT_HOME not defined, cannot build Serpent module.
+
+Ensure the global module makefile checks for this condition before it triggers
+my build.
+================================================================================
+endef
+
+ifeq ($(SERPENT_HOME),)
+  $(error $(MESSAGE_ERROR))
+endif
+
+###############################################################################
+
+# Run the script to make/update a local copy of the Serpent source code
+
+LOCAL_SERPENT_DIR 	:= $(CURDIR)/serpent
+
+TRICK_SCRIPT_TO_RUN	:= $(shell ./scripts/update_serpent.sh $(LOCAL_SERPENT_DIR))
+
+###############################################################################
+
+# Define the build environment
+
+NAME			:= serpent
+SERPENT_LIB		:= $(LOCAL_SERPENT_DIR)/lib$(NAME)-$(METHOD).la
+SERPENT_SRC 		:= $(sort $(wildcard $(LOCAL_SERPENT_DIR)/*.c))
 SERPENT_OBJ		:= $(patsubst %.c, %.$(obj-suffix), $(SERPENT_SRC))
 SERPENT_DEPS		:= $(patsubst %.$(obj-suffix), %.$(obj-suffix).d, $(SERPENT_OBJ))
 
-ADDITIONAL_CPPFLAGS	+= -w -ansi -ffast-math -O3 -DDEBUG -g
-ADDITIONAL_LDFLAGS	+= -lm -lgd -g
+ADDITIONAL_CPPFLAGS	+= -w -ansi -ffast-math -O3
+ADDITIONAL_LDFLAGS	+= -lm
 
-app_INCLUDES		+= -I$(SERPENT_DIR)
+MESSAGE_DIRECTORY	:= - The local copy of Serpent is in '$(LOCAL_SERPENT_DIR)'
+ifeq ($(shell ldconfig -p | grep "libgd\."),)
+  MESSAGE_GRAPHICS	:= - Compiling Serpent without GD graphics support
+  ADDITIONAL_CPPFLAGS	+= -DNO_GFX_MODE
+else
+  MESSAGE_GRAPHICS	:= - GD graphics library found! Compiling Serpent with graphics support
+  ADDITIONAL_LDFLAGS	+= -lgd
+endif
+
+ifeq ($(METHOD),dbg)
+  MESSAGE_MODE	:= - Debug mode deteted, compiling Serpent with debugging options
+  ADDITIONAL_CPPFLAGS	+= -DDEBUG -g
+  ADDITIONAL_LDFLAGS	+= -g
+else
+  MESSAGE_MODE	:= - Compiling Serpent in optimized mode
+endif
+
+app_INCLUDES		+= -I$(LOCAL_SERPENT_DIR)
 app_LIBS		+= $(SERPENT_LIB)
 
 ###############################################################################
 
-# Rule to build the library
+# Create a pretty message that prints out when building Serpent
 
-$(SERPENT_LIB): $(SERPENT_OBJ)
-	@$(libmesh_LIBTOOL) --tag=CC $(LIBTOOLFLAGS) --mode=link --quiet \
-          $(libmesh_CC) $(libmesh_CFLAGS) -o $@ $(SERPENT_OBJ) $(libmesh_LDFLAGS) $(ADDITIONAL_LDFLAGS) $(EXTERNAL_FLAGS) -rpath $(realpath $(SERPENT_DIR))
-	@$(libmesh_LIBTOOL) --mode=install --quiet install -c $(SERPENT_LIB) $(realpath $(SERPENT_DIR))
+define MESSAGE_NOTIFICATION
+================================================================================
+  $(MESSAGE_DIRECTORY)
+  $(MESSAGE_GRAPHICS)
+  $(MESSAGE_MODE)
+================================================================================
+endef
+export MESSAGE_NOTIFICATION
 
 ###############################################################################
+
+# Create the rule to build the Serpent library
+
+$(SERPENT_LIB): pre_install_notifications $(SERPENT_OBJ)
+	@echo "Linking Library "$@"..."
+	@$(libmesh_LIBTOOL) --tag=CC $(LIBTOOLFLAGS) --mode=link --quiet \
+          $(libmesh_CC) $(libmesh_CFLAGS) -o $@ $(SERPENT_OBJ) \
+          $(libmesh_LDFLAGS) $(ADDITIONAL_LDFLAGS) $(EXTERNAL_FLAGS) \
+          -rpath $(realpath $(LOCAL_SERPENT_DIR))
+	@$(libmesh_LIBTOOL) --mode=install --quiet install -c $(SERPENT_LIB) $(realpath $(LOCAL_SERPENT_DIR))
+
+pre_install_notifications:
+	@echo "$$MESSAGE_NOTIFICATION"
+
+###############################################################################
+
+# Add on the the 'clean' and 'clobber' rules to make sure we handle the local
+# Serpent files correctly
 
 clean::
 	@rm -f $(SERPENT_LIB)
@@ -33,9 +97,9 @@ clean::
 	@rm -f $(SERPENT_OBJ)
 
 clobber::
-	@if [ -d "$(SERPENT_DIR)" ]; then\
-		echo Removing directory $(realpath $(SERPENT_DIR)); \
-		rm -rf $(SERPENT_DIR); \
+	@if [ -d "$(LOCAL_SERPENT_DIR)" ]; then\
+		echo Removing directory $(realpath $(LOCAL_SERPENT_DIR)); \
+		rm -rf $(LOCAL_SERPENT_DIR); \
 	fi
 
 ###############################################################################
--- scripts/update_serpent.sh
+++ scripts/update_serpent.sh
@@ -2,30 +2,37 @@
 # This script copies the files from ${SERPENT_HOME} to ./serpent and
 # prepares them to be compile compatibly for use within MOOSE
 
+# Acquire/define the directory for the local copy
 CURRENT_DIR=$(pwd)
-FOLDER=$(basename ${CURRENT_DIR})
-if [ "${FOLDER}" == "scripts" ]; then
-  cd ..
+COPY_TO_DIR=${1:-${CURRENT_DIR}/serpent}
+
+# Ensure our two directories are different
+if [[ ${COPY_TO_DIR} -ef ${SERPENT_HOME} ]]; then
+  printf "Cannot copy when the source and destination directories are the same!\n"
+  exit 1
 fi
-mkdir -p ./serpent
 
-# cd into serpent
-cd serpent
-
-printf "Copying source files from %s to %s... " ${SERPENT_HOME} $(pwd)
-cp ${SERPENT_HOME}/{*.h,*.c} .
-printf "done! \n"
+# Ensure the destination directory exists and then copy
+mkdir -p ${COPY_TO_DIR}
+cd ${COPY_TO_DIR}
+printf "Copying source files from %s to %s... \n" ${SERPENT_HOME} ${COPY_TO_DIR}
+cp -p ${SERPENT_HOME}/{*.h,*.c} .
 
 # Remove invalid beta files
 rm deffcordata.c
 
-printf "Updating .h files with include guard... "
+printf "Patching .h files... \n"
 for file in *.h; do
-  sed -i.old '1s;^;#pragma once\n\n;' ${file}
-done
-printf "done!\n"
+  # Only patch files that are suspect of being unmodified
+  if [[ ! ${file} -nt ${SERPENT_HOME}/${file} ]]; then
+    if [[ "${file}" == "header.h" ]]; then
+      # Put header.h in an extern "C" block
+      sed -i.old '1s;^;#ifdef __cplusplus\nextern "C"{\n#endif\n\n;' header.h
+      printf "#ifdef __cplusplus\n}\n#endif\n" >> header.h
+    fi
 
-printf 'Placing extern "C"{} block on "header.h"... '
-  sed -i.old '1s;^;#ifdef __cplusplus\nextern "C"{\n#endif\n\n;' header.h
-  printf "#ifdef __cplusplus\n}\n#endif\n" >> header.h
-printf "done!\n"
+    # Place an include guard on all header files
+    sed -i.old '1s;^;#pragma once\n\n;' ${file}
+    rm ${file}.old
+  fi
+done
